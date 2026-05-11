4faca44f6023b1463b9eb5af7d6f078ce2fded0e
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2026-05-11 14:54:00 +0300

    modules: tf-m: adapt to TF-PSA-Crypto
    Changes include:
    
    - Updating TF-M revision with changes needed in the repo.
    
    - Removing the unnecessary installation of many PSA Crypto
      header files.
    
    - Adjusting include directories of psa_interface and moving
      them to a dedicated common CMake file instead of
      duplicating them.
    
    - Moving the MBEDTLS_PLATFORM_*_ALT configs from the Mbed TLS
      to the crypto library config file
      (MBEDTLS_PLATFORM_PRINTF_ALT was needed for TF-M,
       MBEDTLS_ENTROPY_HARDWARE_ALT is gone with Mbed TLS 4,
       MBEDTLS_PLATFORM_ZEROIZE_ALT wasn't properly integrated).
    
    - Some cleanup here and there.
    
    Signed-off-by: Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>

---
+++ subsys/nrf_security/cmake/psa_interface_include_directories.cmake
@@ -0,0 +1,15 @@
+#
+# Copyright (c) 2026 Nordic Semiconductor
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+#
+
+target_include_directories(psa_interface
+  INTERFACE
+    ${ZEPHYR_OBERON_PSA_CRYPTO_MODULE_DIR}/include
+    ${ZEPHYR_OBERON_PSA_CRYPTO_MODULE_DIR}/library
+    ${ZEPHYR_OBERON_PSA_CRYPTO_MODULE_DIR}/core
+    ${ZEPHYR_OBERON_PSA_CRYPTO_MODULE_DIR}/dispatch
+    ${ZEPHYR_OBERON_PSA_CRYPTO_MODULE_DIR}/platform
+    ${NRF_SECURITY_DIR}/include
+)
