7c7a0637c57f8f5e432207edeae9f05f26d7331d
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2026-05-12 10:06:55 +0300

    nrf_security: adapt threading to TF-PSA-Crypto
    The struct passed to the mutex functions has changed, so adapt
    accordingly.
    
    As a bonus get rid of the function pointers and just rename the
    functions to the standard names as we don't compile threading.c.
    
    threading_alt.c is now added to the psa_core CMake library because the
    mutex functions are called from e.g. psa_crypto.c and if they are not
    called from other places than this library they get garbage-collected
    before the final link step which results in undefined references to
    everything that threading_alt.c defines.
    
    The duplicate memory_buffer_alloc.c is deleted so that we only use
    the original version from TF-PSA-Crypto.
    It contained some changes specific to CC3XX to make the heap mutex
    variable a pointer.
    This is now made as a noup in oberon-psa-crypto and it applies to
    all cases (not just CC3XX) so we now also define a heap mutex
    in threading_alt.c
    
    Signed-off-by: Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>

--- subsys/nrf_security/src/threading/threading_alt.c
+++ subsys/nrf_security/src/threading/threading_alt.c
@@ -4,38 +4,42 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#include "threading_alt.h"
-#include <stdint.h>
-#include <stddef.h>
-#include <string.h>
-#include <zephyr/init.h>
+#include <mbedtls/threading.h>
 #include <zephyr/kernel.h>
-#include <zephyr/sys/__assert.h>
 
-K_MUTEX_DEFINE(mbedtls_threading_key_slot_mutex);
-K_MUTEX_DEFINE(mbedtls_threading_psa_globaldata_mutex);
-K_MUTEX_DEFINE(mbedtls_threading_psa_rngdata_mutex);
+K_MUTEX_DEFINE(key_slot_mutex);
+K_MUTEX_DEFINE(psa_globaldata_mutex);
+K_MUTEX_DEFINE(psa_rngdata_mutex);
+K_MUTEX_DEFINE(heap_mutex);
 
-void mbedtls_mutex_init_fn(mbedtls_threading_mutex_t *mutex)
+mbedtls_threading_mutex_t mbedtls_threading_key_slot_mutex = {
+	.MBEDTLS_PRIVATE(mutex) = &key_slot_mutex,
+};
+mbedtls_threading_mutex_t mbedtls_threading_psa_globaldata_mutex = {
+	.MBEDTLS_PRIVATE(mutex) = &psa_globaldata_mutex,
+};
+mbedtls_threading_mutex_t mbedtls_threading_psa_rngdata_mutex = {
+	.MBEDTLS_PRIVATE(mutex) = &psa_rngdata_mutex,
+};
+mbedtls_threading_mutex_t mbedtls_threading_heap_mutex = {
+	.MBEDTLS_PRIVATE(mutex) = &heap_mutex,
+};
+
+void mbedtls_mutex_init(mbedtls_threading_mutex_t *mutex)
 {
-	k_mutex_init(mutex);
+	/* No need to do anything, the mutexes are already initialized by K_MUTEX_DEFINE(). */
 }
 
-void mbedtls_mutex_free_fn(mbedtls_threading_mutex_t *mutex)
+void mbedtls_mutex_free(mbedtls_threading_mutex_t *mutex)
 {
 }
 
-int mbedtls_mutex_lock_fn(mbedtls_threading_mutex_t *mutex)
+int mbedtls_mutex_lock(mbedtls_threading_mutex_t *mutex)
 {
-	return k_mutex_lock(mutex, K_FOREVER);
+	return k_mutex_lock(mutex->MBEDTLS_PRIVATE(mutex), K_FOREVER);
 }
 
-int mbedtls_mutex_unlock_fn(mbedtls_threading_mutex_t *mutex)
+int mbedtls_mutex_unlock(mbedtls_threading_mutex_t *mutex)
 {
-	return k_mutex_unlock(mutex);
+	return k_mutex_unlock(mutex->MBEDTLS_PRIVATE(mutex));
 }
-
-void (*mbedtls_mutex_init)(mbedtls_threading_mutex_t *mutex) = mbedtls_mutex_init_fn;
-void (*mbedtls_mutex_free)(mbedtls_threading_mutex_t *mutex) = mbedtls_mutex_free_fn;
-int (*mbedtls_mutex_lock)(mbedtls_threading_mutex_t *mutex) = mbedtls_mutex_lock_fn;
-int (*mbedtls_mutex_unlock)(mbedtls_threading_mutex_t *mutex) = mbedtls_mutex_unlock_fn;
